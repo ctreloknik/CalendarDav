5d6f9213a13060d1422359d2e527d7d219f28c2f
Nikita Chebotaryov <atikin13@rambler.ru>
2016-03-14 21:22:00 +0300

    Добавлен класс для работы с участниками.

---
+++ calendar/cal/src/main/java/ru/nik/beans/MembersBlock.java
@@ -0,0 +1,89 @@
+package ru.nik.beans;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.EJB;
+
+import ru.nik.dto.EventMembersDTO;
+import ru.nik.dto.UserCalendarEventsDTO;
+import ru.nik.dto.UsersDTO;
+import ru.nik.services.servicesImpl.EventmembersServiceBean;
+import ru.nik.services.servicesImpl.UserServiceBean;
+
+/**
+ * @author Nikita
+ *
+ */
+public class MembersBlock
+{
+    @EJB
+    private EventmembersServiceBean membersService;
+    
+    @EJB
+    private UserServiceBean userService;
+    
+    //private List<UsersDTO> users = new ArrayList<UsersDTO>();
+    private List<EventMembersDTO> currentUsers = new ArrayList<EventMembersDTO>();
+    private List<UsersDTO> addedUsers = new ArrayList<UsersDTO>();
+    private List<EventMembersDTO> deletedUsers = new ArrayList<EventMembersDTO>();
+    
+    /*public List<UsersDTO> getUsers()
+    {
+        return users;
+    }
+    public void setUsers(List<UsersDTO> users)
+    {
+        this.users = users;
+    }
+    */
+    
+    public List<EventMembersDTO> getCurrentUsers()
+    {
+        return currentUsers;
+    }
+    public void setCurrentUsers(List<EventMembersDTO> currentUsers)
+    {
+        this.currentUsers = currentUsers;
+    }
+    public List<UsersDTO> getAddedUsers()
+    {
+        return addedUsers;
+    }
+    public void setAddedUsers(List<UsersDTO> addedUsers)
+    {
+        this.addedUsers = addedUsers;
+    }
+    public List<EventMembersDTO> getDeletedUsers()
+    {
+        return deletedUsers;
+    }
+    public void setDeletedUsers(List<EventMembersDTO> deletedUsers)
+    {
+        this.deletedUsers = deletedUsers;
+    }
+
+    public List<UsersDTO> getAllUsers()
+    {
+        return userService.getAll();
+    }
+    
+    public void getCurrentMembersForEvent(Long eventId)
+    {
+        // заглушка для тестов
+        // требуется получать участников для конкретного события
+        currentUsers = membersService.getAll();
+    }
+    
+    public void saveMembers(UserCalendarEventsDTO event)
+    {
+        EventMembersDTO evm;
+        for (UsersDTO user : addedUsers)
+        {
+            evm = new EventMembersDTO();
+            evm.setUser(user);
+            evm.setUserCalendarEventsDTO(event);
+            membersService.create(evm);
+        }
+    }
+}
