4e29cd4a42951941a21f6af17fac27db6e2c4eac
Nikita Chebotaryov <atikin13@rambler.ru>
2016-03-18 18:19:04 +0300

    Codestyle. Добавление новых категорий при апдейте события

--- calendar/db/src/main/java/ru/nik/services/servicesImpl/UserCalendarEventsServiceImpl.java
+++ calendar/db/src/main/java/ru/nik/services/servicesImpl/UserCalendarEventsServiceImpl.java
@@ -18,17 +18,19 @@ import ru.nik.services.UserCalendarEventsService;
  * @author Nikita
  *
  */
-public class UserCalendarEventsServiceImpl extends GenericCrudImpl<UserCalendarEventsDTO, Long> implements UserCalendarEventsService
+public class UserCalendarEventsServiceImpl extends
+        GenericCrudImpl<UserCalendarEventsDTO, Long> implements
+        UserCalendarEventsService
 {
     @PersistenceContext(unitName = "PERSISTENCEUNIT")
     protected EntityManager eman;
-    
+
     @Override
     protected void closeEntityManager()
-	{
-	}
+    {
+    }
 
-	@Override
+    @Override
     protected EntityManager getEntityManager()
     {
         return eman;
@@ -38,73 +40,95 @@ public class UserCalendarEventsServiceImpl extends GenericCrudImpl<UserCalendarE
     {
         super(UserCalendarEventsDTO.class);
     }
-    
+
     @Override
     public UsersDTO getUserEventById(Long userEventId)
     {
         Query q = getEntityManager().createQuery(
-                "select ue.userCalendar.user from UserCalendarEventsDTO ue " +
-                "where ue.userCalendarEventsId=:userEventId");
+                "select ue.userCalendar.user from UserCalendarEventsDTO ue "
+                        + "where ue.userCalendarEventsId=:userEventId");
         q.setParameter("userEventId", userEventId);
         return (UsersDTO) q.getSingleResult();
     }
 
     @Override
-    public void saveCategories(UserCalendarEventsDTO event, List<String> categories)
+    public void saveCategories(UserCalendarEventsDTO event,
+            List<String> categories)
     {
-        create(event);
+        List<EventCategoriesDTO> currentCategories = getEventCategories(event
+                .getUserCalendarEventsId());
+        boolean isExist = false;
         EventCategoriesDTO ec;
-        for (String string : categories)
+        for (String cat : categories)
         {
-            ec = new EventCategoriesDTO();
-            ec.setEvent(event);
-            ec.setCategoryId(EventCategories.getIdByName(string).longValue());
-            eman.merge(ec);
+            for (EventCategoriesDTO curCat : currentCategories)
+            {
+                if (curCat.getCategoryId().equals(EventCategories.getIdByName(cat).longValue()))
+                {
+                    isExist = true;
+                    break;
+                }
+            }
+            if (!isExist)
+            {
+                ec = new EventCategoriesDTO();
+                ec.setEvent(event);
+                ec.setCategoryId(EventCategories.getIdByName(cat).longValue());
+                eman.merge(ec);
+                isExist = false;
+            }
         }
     }
-    
+
     /**
      * Получить события за указанную дату.
-     * @param date дата
+     * 
+     * @param date
+     *            дата
      * @return список событий
      */
     @SuppressWarnings("unchecked")
     @Override
     public List<UserCalendarEventsDTO> getEventsByDate(Date date)
     {
-        Query q = getEntityManager().createQuery(
-                "SELECT e FROM UserCalendarEventsDTO e " +
-                "WHERE :date BETWEEN e.startDatetime AND e.endDatetime");
+        Query q = getEntityManager()
+                .createQuery(
+                        "SELECT e FROM UserCalendarEventsDTO e "
+                                + "WHERE :date BETWEEN e.startDatetime AND e.endDatetime");
         q.setParameter("date", date);
         return q.getResultList();
     }
-    
+
     /**
      * Получение ближайших событий.
+     * 
      * @return
      */
     @SuppressWarnings("unchecked")
     @Override
     public List<UserCalendarEventsDTO> getNextEvents()
     {
-        Query q = getEntityManager().createQuery(
-                "SELECT DISTINCT e from UserCalendarEventsDTO e "
-                + "WHERE e.startDatetime > current_date ORDER BY e.startDatetime");
+        Query q = getEntityManager()
+                .createQuery(
+                        "SELECT DISTINCT e from UserCalendarEventsDTO e "
+                                + "WHERE e.startDatetime > current_date ORDER BY e.startDatetime");
         return q.getResultList();
     }
-    
+
     /**
      * Получить категории события по ид события.
-     * @param eventId ид события
+     * 
+     * @param eventId
+     *            ид события
      * @return категории.
      */
     public List<EventCategoriesDTO> getEventCategories(Long eventId)
     {
         Query q = getEntityManager().createQuery(
                 "SELECT ec from EventCategoriesDTO ec "
-                + "WHERE ec.event.userCalendarEventsId =:eventId");
+                        + "WHERE ec.event.userCalendarEventsId =:eventId");
         q.setParameter("eventId", eventId);
         return q.getResultList();
     }
-    
+
 }
