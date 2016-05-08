859003cecd7a27df7b81618457caea980d58279a
Nikita Chebotaryov <atikin13@rambler.ru>
2016-05-08 22:55:55 +0300

    Небольшая доработка интерфейса

--- calendar/cal/src/main/java/ru/nik/beans/EventHome.java
+++ calendar/cal/src/main/java/ru/nik/beans/EventHome.java
@@ -63,7 +63,7 @@ public class EventHome implements Serializable
 
     public String logout(){
         FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
-        return "success";
+        return "Basic";
     }
     
     @PostConstruct
